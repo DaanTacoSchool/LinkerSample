5801b5c259c763fb144a83688952d58a2f99c179
daan <dvanessen@quistor.com>
2019-05-13 10:47:22 +0200

    Add project files.

---
+++ LinkerSample/LinkerSample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ContextMenu;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LinkerSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SampleList.ItemsSource = Enumerable.Range(0, 300);
+        }
+        private void OnClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            DisplayAlert($"{button.CommandParameter} clicked", null, "OK");
+
+           // Device.BeginInvokeOnMainThread(() => GetParent<SideActionBarCell>(button, button.Parent).ForceClose());
+        }
+
+        private void OnOpenClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+          //  Device.BeginInvokeOnMainThread(() => GetParent<SideActionBarCell>(button, button.Parent).ForceOpen());
+        }
+    }
+}
